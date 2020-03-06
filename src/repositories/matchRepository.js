8dcbb5266140fbaf207b12bbe756d9a6b6b36d81
Taylor McGann <taylormcgann1@gmail.com>
2020-03-06 16:39:21 -0700

    Backup data before overwriting authoritative file

--- src/repositories/matchRepository.js
+++ src/repositories/matchRepository.js
@@ -1,6 +1,7 @@
 const { loadData, writeData } = require('../db')
 
 const DATA_PATH = './src/data/matches.json'
+const DATA_BAK_PATH = './src/data/matches.bak.json'
 
 function getMatches() {
   return loadData(DATA_PATH)
@@ -8,6 +9,8 @@ function getMatches() {
 
 function updateMatches(matches, exclusions) {
   const existingMatches = getMatches()
+  writeData(DATA_BAK_PATH, existingMatches)
+
   const combinedMatches = existingMatches.slice()
   combinedMatches.unshift({
     createdAt: new Date().toISOString(),
--- src/repositories/personRepository.js
+++ src/repositories/personRepository.js
@@ -2,6 +2,7 @@ const { loadData, writeData } = require('../db')
 const { getId, sortByFirstName } = require('../utils/person')
 
 const DATA_PATH = './src/data/persons.json'
+const DATA_BAK_PATH = './src/data/persons.bak.json'
 
 function createPerson(firstName, lastName, email) {
   return {
@@ -17,11 +18,14 @@ function getPersons() {
 }
 
 function updatePersons(personsChanged) {
-  const personsChangedIdSet = new Set(personsChanged.map(person => getId(person)))
   const existingPersons = getPersons()
+  writeData(DATA_BAK_PATH, existingPersons)
+
+  const personsChangedIdSet = new Set(personsChanged.map(person => getId(person)))
   const personsUnchanged = existingPersons.filter(person => !personsChangedIdSet.has(getId(person)))
   const combinedPersons = [...personsChanged, ...personsUnchanged]
   combinedPersons.sort(sortByFirstName)
+
   writeData(DATA_PATH, combinedPersons)
 }
 
